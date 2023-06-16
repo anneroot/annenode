cefa95d0b16a522b7f98d10fe22e66449382d24f
Anne <anneyroh@gmail.com>
2023-06-16 16:07:08 -0300

    Projeto node js

---
+++ AULA01/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.get('/', (req, res) => {
+ 
+})
+
+
+
+//webserver
+app.listen(3000, () => {
+  console.log('Server Started')
+})
